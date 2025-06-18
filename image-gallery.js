6f0aa14b57004072f5c50892f95f94654ae97724
Danielle-Morvant <daniellemorvant1@gmail.com>
2025-06-17 23:52:31 -0500

    edited

--- image-gallery.js
+++ image-gallery.js
@@ -1,19 +1,20 @@
 const gallery = document.getElementById("gallery");
 
-  // Fetch 12 random images from Lorem Picsum
+// Fetch 12 random images from Lorem Picsum
 async function fetchImages() {
   try {
-
     // CONNECTS TO THE WEBSITE AND STORES THE RESULTS IN RES
     const res = await fetch("https://picsum.photos/v2/list?page=2&limit=50");
 
     //CONVERTS THE RESULTS FROM STRING FORM TO OBJECT OR DATA FORM
     const data = await res.json();
 
-    // CONVERTS THE DATA TO HTML AND INSERTS IT INTO THE gallery ID TAG 
-    gallery.innerHTML = data.map((image) => {
+    // CONVERTS THE DATA TO HTML AND INSERTS IT INTO THE gallery ID TAG
+    gallery.innerHTML = data
+      .map((image) => {
         return ` 
-          <div class = "rounded-lg shadow-md shadow-cyan-700 overflow-hidden w-full h-60 object-cover m-3 hover:scale-110 duration-300 ease-in-out">
+          <div class = "rounded-lg shadow-md shadow-cyan-700 overflow-hidden w-full h-60 object-cover m-3 hover:scale-110 duration-300 ease-in-out
+          ">
             <img src="${image.download_url}" alt="Random image by ${image.author}" />
           </div>`;
       })
